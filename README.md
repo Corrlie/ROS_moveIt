12dad4a31e67772bcfe162bc59037c4a1717e89e
Corrlie <wojciech.konkol1@gmail.com>
2021-03-08 11:18:37 +0100

    Added comments in python file; Modified readme file - added description and images

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ROS_moveIt
-Python file to ROS project - moving panda robot to predefined positions - using moveIt package
+
+ROS distribution: ROS Noetic.
+OS: Linux Ubuntu 20.04.1 LTS on Oracle VM VirtualBox 
+
+ROS project - moving panda robot to predefined positions - using Python and moveIt package. It is visualized in RViz environment.
+
+The repo excludes all ROS files (launch files etc.).
+
+Running the python file via terminal command "python3 ros_moveit_isa_project.py". After this command terminal looks like on the image below:
+
+![terminal](https://user-images.githubusercontent.com/63510085/110304267-aa8fc600-7ffb-11eb-8bfd-ebd2cfd1734f.png)
+
+All prompts are in the Polish language and are translated below.
+In the terminal there are displayed current joints positions (pol. "Aktualne ustawienie przegubow"), title of the project and short menu. There are three options in the menu: 1. Movement to default configuration (pol. "Ruch do konfiguracji domyslnej"); 2. Movement to predefined configuration (pol. "Ruch do zdefiniowanej konfiguracji"); 3. Close the program (pol. "Zakoncz program"). Below the menu there is request to choose the option (pol. "Wybierz opcje: ").
+
+Choosing the first option of menu:
+![option1_menu](https://user-images.githubusercontent.com/63510085/110305217-cba4e680-7ffc-11eb-9b80-7dfd0c2030e1.png)
+There is a prompt about chosen option - to default configuration (pol. "Wybrano ruch do konfiguracji domyslnej"). Then "Be patient, the movement is being performed" prompt (pol. "Cierpliwosci... trwa ruch do domyslnej konfiguracji"). In the RViz environment One can see that robot is performing chosen movement at the moment. 
+After that there is also the list of joints positions after the movement (pol. "Stan po ruchu: "). In the end there is the text that movement was finished (pol. "Ruch zakonczony") and about going back to the menu (pol. "Powrot do menu"). Image below shows how the robot looks after performing the movement (while there is prompt about ongoing movement in the terminal).
+![option1_robot](https://user-images.githubusercontent.com/63510085/110305247-d6f81200-7ffc-11eb-9017-b421379e8289.png)
+
+
+Choosing the second option of menu - prompts analogous to the prompts from the first option of the menu:
+![option2_menu](https://user-images.githubusercontent.com/63510085/110305312-eb3c0f00-7ffc-11eb-8712-a3cce9ea78e6.png)
+![option2_robot](https://user-images.githubusercontent.com/63510085/110305422-f42ce080-7ffc-11eb-95ae-c09a3c2b4e00.png)
+
+Choosing the third option of menu:
+![option3_menu](https://user-images.githubusercontent.com/63510085/110305489-06a71a00-7ffd-11eb-9b39-3b6473002c3a.png)
+
+Third option closes the program.
+
+
--- ros_moveit_isa_project.py
+++ ros_moveit_isa_project.py
@@ -7,8 +7,6 @@ from math import pi
 class ROS_isa_moveit_project:
 	def __init__(self):
 		moveit_namespace.roscpp_initialize(sys.argv)
-		# rospy.init_node('move_group', anonymous=True)
-		# args init mode: (name of node; anonymous=true - ensures node has unique name)
 		robot_info = moveit_namespace.RobotCommander() # kinematic and states
 		scene_perception= moveit_namespace.PlanningSceneInterface()
 		robot_name = "panda_arm"
@@ -17,11 +15,12 @@ class ROS_isa_moveit_project:
 		self.scene_perception = scene_perception
 		self.robot_name = robot_name
 		self.move_robot = move_robot     
-		print('Aktualne ustawienie przegubow:')
-		print(self.move_robot.get_current_joint_values())
+		print('Aktualne ustawienie przegubow:') # current joints position
+		print(self.move_robot.get_current_joint_values()) # current joints position
 
-	def go_default(self):
+	def go_default(self): # go to default configuration
 		def_swivel = self.move_robot.get_current_joint_values()
+        # positions of newly spawned robot in environment:
 		def_swivel[0] = 0 
 		def_swivel[1] = -0.785
 		def_swivel[2] = 0
@@ -32,11 +31,12 @@ class ROS_isa_moveit_project:
 		self.move_robot.go(def_swivel, wait=True)
 		self.move_robot.stop()
 		current_pos_def_swiv = self.move_robot.get_current_joint_values()
-		print('Stan po ruchu: \n')
+		print('Stan po ruchu: \n') # position after the movement
 		print(current_pos_def_swiv)
        
-	def go_predefined(self):
+	def go_predefined(self): # go to predefined exemplary configuration
 		swivel = self.move_robot.get_current_joint_values()
+        # predefined exemplary positions:
 		swivel[0] = -pi/6
 		swivel[1] = -pi/6
 		swivel[2] = -pi/6
@@ -49,23 +49,23 @@ class ROS_isa_moveit_project:
 		self.move_robot.stop()
 		
 		current_pos_swiv = self.move_robot.get_current_joint_values()
-		print('Stan po ruchu: \n')
+		print('Stan po ruchu: \n') # Position after the movement
 		print(current_pos_swiv)
+        
 def menu_fun(move_it_obj):
-	
 	print("------------------------MENU------------------------")
-	print("1. Ruch do konfiguracji domyslnej")
-	print("2. Ruch do zdefiniowanej konfiguracji")
-	print("3. Zakoncz program")
-	option_menu = int(input("Wybierz opcje: "))
+	print("1. Ruch do konfiguracji domyslnej") # movement to default configuration
+	print("2. Ruch do zdefiniowanej konfiguracji") # movement to predefined configuration
+	print("3. Zakoncz program") # close program
+	option_menu = int(input("Wybierz opcje: ")) # choose menu option
 	while option_menu!=0:
-		if option_menu == 1:
-			print("Wybrano ruch do konfiguracji domyslnej")
+		if option_menu == 1: 
+			print("Wybrano ruch do konfiguracji domyslnej") # "movement to default configuration was chosen" prompt
 			print("...")
-			print("Cierplowosci... trwa ruch do domyslnej konfiguracji...")
+			print("Cierplowosci... trwa ruch do domyslnej konfiguracji...") # "Be patient, the movement is being performed" prompt
 			move_it_obj.go_default()
-			print("Ruch zakonczony")
-			print("\nPowrot do menu...\n\n")
+			print("Ruch zakonczony") # "Finished movement" prompt
+			print("\nPowrot do menu...\n\n") # "Going back to the menu" prompt
 		elif option_menu == 2:
 			print("Wybrano ruch do zdefiniowanej konfiguracji")
 			print("...")
@@ -74,7 +74,7 @@ def menu_fun(move_it_obj):
 			print("Ruch zakonczony")
 			print("\nPowrot do menu...\n\n")
 		elif option_menu == 3:
-			print("Trwa zamykanie programu...")
+			print("Trwa zamykanie programu...") # closing the program
 			exit()
 		break
 def main():
